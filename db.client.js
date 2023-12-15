df38511b8fb167e77cabdd53eb92768ed935bcb3
mamacito93 <vellencunee@gmail.com>
2023-12-15 11:16:36 +0100

    push

--- db.client.js
+++ db.client.js
@@ -2,7 +2,12 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  
+  'db_cloud_b3sj', // db name
+  'db_cloud_b3sj_user', // user
+  '6wbCAjJB21Dv8vPZWkapPue32HiD6tzE', // password
+  {
+    host : 'dpg-clu1lvq1hbls73e8c5r0-a',
+    dialect : 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
